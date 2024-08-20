b2bfa6ae71a7af6bd6e57b4d7b1ee629db79f761
Francisco Rodriguez <f51783145@gmail.com>
2024-08-20 11:23:09 -0600

    Actualizacion de Estructura 2

--- Cliente/src/app/auth.guard.spec.ts
+++ Cliente/src/app/auth.guard.spec.ts
@@ -1,9 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { CanActivateFn } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 
-import { authGuard } from './auth.guard';
-
-describe('authGuard', () => {
+describe('AuthGuard', () => {
   const executeGuard: CanActivateFn = (...guardParameters) => 
       TestBed.runInInjectionContext(() => authGuard(...guardParameters));
 
